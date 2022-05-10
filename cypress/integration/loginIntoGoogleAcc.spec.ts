0eaae04f62abecd0640673afcb547afd42a56688
unknown <you@ex>
2022-05-10 13:46:38 +0300

    making corrections to exercise

--- cypress/integration/loginIntoGoogleAcc.spec.ts
+++ cypress/integration/loginIntoGoogleAcc.spec.ts
@@ -1,28 +1,36 @@
-describe('the test', () => {
+describe('testing the login into Google accounts process', () => {
 
     beforeEach(() => {
         cy.fixture('tryout').as('workplz')
     })
     it('going to sign in page', () => {
         cy.visit('https://www.google.co.il/')
-        cy.get('.lnXdpd', {timeout: 10000}).should('be.visible')
-        cy.emailAdd('Google Account', '.gLFyf.gsfi')
-        cy.get('.gNO89b').first().click()
-        cy.get('.l').first().should('have.attr', 'href', 'https://account' +
+        const searchButton = '.gNO89b'
+        const inputInBar = '.gLFyf.gsfi'
+        const searchBar = '.lnXdpd'
+        const seacrchResults = 'l'
+        cy.get(searchBar, {timeout: 10000}).should('be.visible')
+        cy.typeInElement('Google Account', inputInBar)
+        cy.get('searchButton').first().click()
+        cy.get(seacrchResults).first().should('have.attr', 'href', 'https://account' +
             's.google.com/ServiceLogin?service=accountsettings')
     })
 
     it('Signing into account', () => {
+        const emailInput = '#identifierId'
+        const nextBtn = '#identifierId'
+        const showPassword = '.sSzDje.NEk0Ve'
         cy.visit('https://accounts.google.com/ServiceLogin?service=accountsettings')
-        cy.get('#identifierId').type(this.workplz.email)
-        cy.get('.VfPpkd-vQzf8d').contains('הבא').should('have.text', 'הבא').click()
+        cy.get(emailInput).type(this.workplz.email)
+        cy.get(nextBtn).contains('הבא').should('have.text', 'הבא').click()
+        cy.get(showPassword).should('be.visible')
     })
 
-    it('just do it!', () => {
-        cy.visit('https://www.w3schools.com/')
-        cy.get('#navbtn_exercises').click()
-        cy.get('.w3-bar-item.w3-button').each(($el) => {
-            expect($el).to.be.visible()
-        })
-    })
+    // it('just do it!', () => {
+    //     cy.visit('https://www.w3schools.com/')
+    //     cy.get('#navbtn_exercises').click()
+    //     cy.get('.w3-bar-item.w3-button').each(($el) => {
+    //         expect($el).to.be.visible()
+    //     })
+    // })
 })
--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,23 +1,5 @@
-// ***********************************************************
-// This example support/index.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
-// Import commands.js using ES2015 syntax:
 import './commands'
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
 declare global {
     namespace Cypress {
         interface Chainable {
@@ -25,7 +7,7 @@ declare global {
              * Custom command to select DOM element by data-cy attribute.
              * @example cy.dataCy('greeting')
              */
-            emailAdd(value: string, string): Chainable<Element>
+            typeInElement(value: string, string): Chainable<Element>
         }
     }
 }
