5fe885acf2e2f868e4833c3f88f9872c44eadd4c
stefanonono23 <78358416+stefanonono23@users.noreply.github.com>
2022-05-10 10:03:17 +0300

    Revert "delete everything"

---
+++ cypress/integration/getFixture.spec.ts
@@ -0,0 +1,11 @@
+describe('Login to OrangeHRM website', () => {
+    before(function () {
+        cy.fixture('example').then(function (data) {
+          this.data = data;
+        })
+    })
+
+    it('Validate successful Login', function () {
+     cy.visit('https://opensource-demo.orangehrmlive.com/')
+    })
+})
