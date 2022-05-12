a767abf110eb55904e053d89897385b00978d962
unknown <you@ex>
2022-05-12 13:49:56 +0300

    adding changes to practice spy/stub tests

--- cypress/integration/interceptPractice.spec.ts
+++ cypress/integration/interceptPractice.spec.ts
@@ -1,15 +1,18 @@
 describe('test intercepting requests', function () {
 
     beforeEach(() => {
-        cy.visit('https://www.youtube.com/result' +
-            's?search_query=%D7%94%D7%A4%D7%99%D7%92%27%D7%9E%D7%95%D7%AA')
+        cy.visit('https://www.w3schools.com/', {
+            onBeforeLoad(win) {
+                cy.stub(win, 'prompt').returns('my custom message')
+            }
+        })
         cy.intercept('GET', '**/*.js').as('allJsFiles')
     })
 
     it('tryna find javascript files in request', function () {
-        cy.wait('@allJsFiles').its('response.statusCode').should('eq', 200)
-        cy.window().then(win => {
-            cy.spy(win, 'prompt')
-        })
+        // cy.wait('@allJsFiles').its('response.statusCode').should('eq', 200)
+        cy.get('#search2').type('wassap')
+        cy.window().its('prompt').should('be.called')
+
     })
 });
--- cypress/integration/openinigNewFile.spec.ts
+++ cypress/integration/openinigNewFile.spec.ts
@@ -1,16 +1,20 @@
 
 describe('opening new tab and spy/stub pracitice', function () {
 
-    it('opening new tab', () => {
-        cy.visit('https://the-internet.herokuapp.com/windows')
-        cy.contains('Click Here').invoke('removeAttr', 'target').click()
-        cy.url().then(uri => {
-            expect(uri).to.contain('/windows/new')
-        })
-        cy.get('h3').should('have.text', 'New Window')
-    })
+    // it('opening new tab', () => {
+    //     cy.visit('https://the-internet.herokuapp.com/windows')
+    //     cy.contains('Click Here').invoke('removeAttr', 'target').click()
+    //     cy.url().then(uri => {
+    //         expect(uri).to.contain('/windows/new')
+    //     })
+    //     cy.get('h3').should('have.text', 'New Window')
+    // })
 
     it('trying out stub/spy', () => {
-
+        cy.visit('https://the-internet.herokuapp.com/windows')
+        cy.stub().as('open')
+        cy.contains('Click Here').click()
+        cy.get('@open').should('have.been.calledOnce')
     })
-});
+
+})
